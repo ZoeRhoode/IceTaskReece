e3c41c46083040aedb2f7a2176b753ac68372f11
Zoe Rhoode <ST10085577@vcconnect.edu.za>
2023-04-12 11:42:02 +0000

    added new code

--- IceTaskReece/Program.cs
+++ IceTaskReece/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("hello class, how is the weather?");
         }
     }
 }
